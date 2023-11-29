f07eececa4b9a86c8dd51dc437c63e24b190cfba
IASIKO <iaseshviligi@gmail.com>
2023-11-29 14:46:32 +0400

    update funcs

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,53 @@
-import { useState } from "react";
 import Header from "./components/Header";
 import Keys from "./components/Keys";
 import Screen from "./components/Screen";
 
-
 function App() {
-  const [theme, setTheme] = useState()
-  
+  const onOne = () => {};
+  const onTwo = () => {};
+  const onThree = () => {};
+  const onFour = () => {};
+  const onFive = () => {};
+  const onSix = () => {};
+  const onSeven = () => {};
+  const onEight = () => {};
+  const onNine = () => {};
+  const onZero = () => {};
+  const onDelete = () => {};
+  const onReset = () => {};
+  const onPlus = () => {};
+  const onMinus = () => {};
+  const onTimes = () => {};
+  const onDivision = () => {};
+  const onEquals = () => {};
+  const onDot = () => {};
+
+
   return (
     <div className="w-screen h-screen bg-primary-main-bg items-center flex-col justify-center">
-      <main className="w-[440px] h-[575px] font-spartan p-[20px] border-[1px] border-primary-key-bg2 rounded-[5px] m-auto translate-y-24">
+      <main className="w-[440px] h-[575px] font-spartan p-[20px] border-[1px] border-primary-key-bg2 rounded-[5px] m-auto translate-y-14">
         <Header />
         <Screen />
-        <Keys />
+        <Keys
+          onOne={onOne}
+          onTwo={onTwo}
+          onThree={onThree}
+          onFour={onFour}
+          onFive={onFive}
+          onSix={onSix}
+          onSeven={onSeven}
+          onEight={onEight}
+          onNine={onNine}
+          onZero={onZero}
+          onDelete={onDelete}
+          onReset={onReset}
+          onPlus={onPlus}
+          onMinus={onMinus}
+          onTimes={onTimes}
+          onDivision={onDivision}
+          onEquals={onEquals}
+          onDot={onDot}
+        />
       </main>
     </div>
   );
--- src/components/Keys.jsx
+++ src/components/Keys.jsx
@@ -1,60 +1,133 @@
 import React from "react";
 
-const Keys = () => {
+const Keys = ({
+  onOne,
+  onTwo,
+  onThree,
+  onFour,
+  onFive,
+  onSix,
+  onSeven,
+  onEight,
+  onNine,
+  onZero,
+  onDelete,
+  onReset,
+  onPlus,
+  onMinus,
+  onTimes,
+  onDivision,
+  onEquals,
+  onDot,
+}) => {
   return (
     <div className="w-[400px] h-[360px] rounded-[5px] bg-primary-keypad-toggle-bg p-[20px] grid grid-cols-4 gap-[20px]">
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onSeven}
+      >
         7
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onEight}
+      >
         8
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onNine}
+      >
         9
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg2 shadow-[0_4px_0] shadow-primary-key-shadow2  text-[23px]  text-primary-screen hover:bg-primary-hover2 hover:shadow-primary-hover-bg2 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg2 shadow-[0_4px_0] shadow-primary-key-shadow2  text-[23px]  text-primary-screen hover:bg-primary-hover2 hover:shadow-primary-hover-bg2 transition-all duration-[200ms]"
+        onClick={onDelete}
+      >
         DEL
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onFour}
+      >
         4
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onFive}
+      >
         5
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onSix}
+      >
         6
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onPlus}
+      >
         &#43;
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onOne}
+      >
         1
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onTwo}
+      >
         2
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onThree}
+      >
         3
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onMinus}
+      >
         &#8722;
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onDot}
+      >
         &#8901;
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onZero}
+      >
         0
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onDivision}
+      >
         &#247;
       </button>
-      <button className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]">
+      <button
+        className="rounded-[8px] bg-primary-key-bg-main shadow-[0_4px_0] shadow-primary-key-shadow-main text-[32px] hover:bg-primary-hover1 hover:shadow-primary-hover-bg1 transition-all duration-[200ms]"
+        onClick={onTimes}
+      >
         &#215;
       </button>
-      <button className="col-[1/3] rounded-[8px] bg-primary-key-bg2 shadow-[0_4px_0] shadow-primary-key-shadow2 text-[23px] text-primary-screen hover:bg-primary-hover2 hover:shadow-primary-hover-bg2 transition-all duration-[200ms]">
+      <button
+        className="col-[1/3] rounded-[8px] bg-primary-key-bg2 shadow-[0_4px_0] shadow-primary-key-shadow2 text-[23px] text-primary-screen hover:bg-primary-hover2 hover:shadow-primary-hover-bg2 transition-all duration-[200ms]"
+        onClick={onReset}
+      >
         RESET
       </button>
-      <button className="col-[3/-1] rounded-[8px] bg-primary-key-bg3-toggle shadow-[0_4px_0] shadow-primary-key-shadow3 text-[32px]  text-primary-screen hover:bg-primary-hover3 hover:shadow-primary-hover-bg3 transition-all duration-[200ms]">
+      <button
+        className="col-[3/-1] rounded-[8px] bg-primary-key-bg3-toggle shadow-[0_4px_0] shadow-primary-key-shadow3 text-[32px]  text-primary-screen hover:bg-primary-hover3 hover:shadow-primary-hover-bg3 transition-all duration-[200ms]"
+        onClick={onEquals}
+      >
         &#61;
       </button>
     </div>
